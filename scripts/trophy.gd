26fd65b0ea7d5fee2938655524743aa7b6e6e5cc
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-25 06:01:05 +0700

    add finish and restarting feature

---
+++ scripts/trophy.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+
+@onready var game_manager = $"../GameManager"
+
+func _ready() -> void:
+	if game_manager.points < 40:
+		visible = false
+		get_node("CollisionShape2D").set_deferred("disabled", true) 
+	else :
+		visible = true
+		get_node("CollisionShape2D").set_deferred("disabled", false) 
+
+func _process(delta: float) -> void:
+	if game_manager.points >= 40:
+		visible = true
+		get_node("CollisionShape2D").set_deferred("disabled", false) 
+		set_process(false)
+		
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		queue_free()
+		print("You Win")
+		get_tree().reload_current_scene()
