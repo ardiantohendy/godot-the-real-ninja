5240cc3ae35648959d2d33292c45b7089f0ee591
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-26 03:56:09 +0700

    adding dead animation (still error)

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
 
-
+@onready var animated_sprite_2d: AnimatedSprite2D
+func _ready():
+	print("AnimatedSprite2D:", animated_sprite_2d)  # Cek apakah null
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "CharacterBody2D":
-		print("YOU DEAD")
-		get_tree().reload_current_scene()
+		if animated_sprite_2d:
+			animated_sprite_2d.play("dead")
+		else:
+			print("ERROR: animated_sprite_2d is null!")
+		print("You Dead!")
