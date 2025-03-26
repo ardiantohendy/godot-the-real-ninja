4622d7e226f4da54128114df291beedf18c8f58c
Ardiantohendy <bayuardianto421@gmail.com>
2025-03-26 17:03:45 +0700

    adding new trap = saw2

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,11 +3,11 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	
-	print("You Dead")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+	if body.name == "CharacterBody2D":
+		print("You Dead")
+		Engine.time_scale = 0.5
+		body.get_node("CollisionShape2D").queue_free()
+		timer.start()	
 
 func _on_timer_timeout() -> void:
 	print("Restarting game...")
